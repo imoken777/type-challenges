093b9078b913c2c9eb8496387c7910c89dfa9140
imoken777 <imoken27@gmail.com>
2024-08-09 18:38:46 +0900

    00015

---
+++ 00015-medium-last.ts
@@ -0,0 +1,14 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<Last<[2]>, 2>>,
+  Expect<Equal<Last<[3, 2, 1]>, 1>>,
+  Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>
+];
+
+// ============= Your Code Here =============
+// type Last<T extends any[]> = {
+//   [P in T[number]]: P;
+// }[3];
+type Last<T extends any[]> = [any, ...T][T["length"]];
