ff96d9dbd316266c3119f70ba868b2ddb83f33ee
imoken777 <imoken27@gmail.com>
2024-12-29 00:04:40 +0900

    02822

---
+++ hard/02822-hard-split.ts
@@ -0,0 +1,56 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "../test-utils";
+
+type cases = [
+  Expect<Equal<Split<"Hi! How are you?">, ["Hi! How are you?"]>>,
+  Expect<Equal<Split<"Hi! How are you?", "z">, ["Hi! How are you?"]>>,
+  Expect<Equal<Split<"Hi! How are you?", " ">, ["Hi!", "How", "are", "you?"]>>,
+  Expect<
+    Equal<
+      Split<"Hi! How are you?", "">,
+      [
+        "H",
+        "i",
+        "!",
+        " ",
+        "H",
+        "o",
+        "w",
+        " ",
+        "a",
+        "r",
+        "e",
+        " ",
+        "y",
+        "o",
+        "u",
+        "?"
+      ]
+    >
+  >,
+  Expect<Equal<Split<"", "">, []>>,
+  Expect<
+    Equal<Split<"The sine in cosine", "in">, ["The s", "e ", " cos", "e"]>
+  >,
+  Expect<
+    Equal<
+      Split<"Never say never, forever and ever.", "ver">,
+      ["Ne", " say ne", ", fore", " and e", "."]
+    >
+  >,
+  Expect<Equal<Split<"", "z">, [""]>>,
+  Expect<Equal<Split<"">, [""]>>,
+  Expect<Equal<Split<string, "whatever">, string[]>>
+];
+
+// ============= Your Code Here =============
+type Split<
+  S extends string,
+  SEP extends string | undefined = undefined
+> = string extends S
+  ? string[]
+  : S extends `${infer L}${SEP}${infer R}`
+  ? [L, ...(R extends "" ? [] : Split<R, SEP>)]
+  : SEP extends ""
+  ? []
+  : [S];
